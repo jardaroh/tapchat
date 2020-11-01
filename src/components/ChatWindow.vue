8d441d48a001234ad2dea68c3579de14592aa52d
Jardar <n00t@pop-os.localdomain>
2020-11-01 22:14:47 +0100

    Added styling to the main of the chat window

--- src/components/ChatWindow.vue
+++ src/components/ChatWindow.vue
@@ -1,7 +1,16 @@
 <template>
   <div v-if="chat" class="ChatWrapper" @click="chat = null">
     <div class="ChatWindow" @click.stop>
-      hello
+      <div class="header">{{ chat.participants.them.username }}</div>
+      <div class="body"></div>
+      <div class="footer">
+        <label>
+          <input type="text"
+                 :placeholder="`write a message to ${chat.participants.them.username}`"
+          >
+        </label>
+        <button>Send</button>
+      </div>
     </div>
   </div>
 </template>
@@ -29,10 +38,39 @@ export default {
 
   .ChatWindow {
     position: absolute;
+    min-width: 40rem;
+    max-width: 80vw;
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
-    background: white;
+    box-shadow:
+      8px 8px 8px #0002,
+      -8px -8px 8px #0002,
+      -8px 8px 8px #0002,
+      8px -8px 8px #0002
+    ;
+    .header {
+      background: var(--blue-darker);
+      color: #fff;
+      padding: 1rem;
+      text-transform: uppercase;
+    }
+    .body {
+      height: 60vh;
+      padding: 1rem;
+      overflow-y: scroll;
+      background: #fff;
+    }
+    .footer {
+      display: grid;
+      grid-template-columns: auto max-content;
+      background: var(--blue-darker);
+      padding: 0.5rem;
+      gap: 0.5rem;
+      input {
+        width: 100%;
+      }
+    }
   }
 }
 </style>
