397c854a1e119736ad165354c4abaa1907d38db4
Jardar <n00t@pop-os.localdomain>
2020-11-01 23:45:26 +0100

    Fixed an issue where the roomname of a chat was incomplete, history fetch failing

--- src/composition/useUser.js
+++ src/composition/useUser.js
@@ -1,11 +1,11 @@
 import { ref, watch } from 'vue';
 
 const SIGNED_IN_USER = 'signedInUser';
-const signedInUser = ref(localStorage.getItem(SIGNED_IN_USER));
+const signedInUser = ref(JSON.parse(localStorage.getItem(SIGNED_IN_USER)));
 
-watch(signedInUser, () => {
-  if (signedInUser.value) {
-    localStorage.setItem(SIGNED_IN_USER, JSON.stringify(signedInUser.value));
+watch(signedInUser, (newVal) => {
+  if (newVal) {
+    localStorage.setItem(SIGNED_IN_USER, JSON.stringify(newVal));
   } else {
     localStorage.removeItem(SIGNED_IN_USER);
   }
