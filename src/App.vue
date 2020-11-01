cedf18010ee42ece526478d1ddb2fc1411d4fc22
Jardar <n00t@pop-os.localdomain>
2020-11-01 20:54:12 +0100

    Styled up the main frames of the application

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <login-form v-if="!signedInUser"></login-form>
-  <div v-else id="#App">
-    <button @click="signedInUser = null">Sign out</button>
+  <div v-else id="Main">
+    <header>
+      <button @click="signedInUser = null">Sign out</button>
+    </header>
+    <main></main>
+    <footer></footer>
   </div>
 </template>
 
@@ -24,4 +28,40 @@ export default {
 </script>
 
 <style lang="scss">
+:root {
+  --blue-light: #BEE3F8;
+  --blue-darker: #3182CE;
+}
+* {
+  box-sizing: border-box;
+}
+html, body {
+  padding: 0;
+  margin: 0;
+  width: 100vw;
+  height: 100vh;
+}
+#app {
+  height: 100vh;
+}
+#Main {
+  display: grid;
+  grid-template-rows: max-content auto max-content;
+  height: 100vh;
+
+  header, footer {
+    display: grid;
+    width: 100vw;
+    height: 5rem;
+    padding: 1rem;
+    background: var(--blue-darker);
+  }
+  header, button {
+    place-self: center end;
+  }
+  main {
+    display: grid;
+    grid-template-columns: auto max-content;
+  }
+}
 </style>
